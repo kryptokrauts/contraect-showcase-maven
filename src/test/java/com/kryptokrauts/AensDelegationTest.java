0dd10e69c61a5a5ac0d09fbb4188546747a0cf34
marc0olo <walz.marcoo@gmail.com>
2021-07-07 21:23:55 +0200

    feature: examples for delegation signatures

---
+++ src/test/java/com/kryptokrauts/AensDelegationTest.java
@@ -0,0 +1,169 @@
+package com.kryptokrauts;
+
+import com.kryptokrauts.aeternity.sdk.constants.AENS;
+import com.kryptokrauts.aeternity.sdk.constants.Network;
+import com.kryptokrauts.aeternity.sdk.domain.secret.KeyPair;
+import com.kryptokrauts.aeternity.sdk.service.ServiceConfiguration;
+import com.kryptokrauts.aeternity.sdk.service.account.domain.AccountResult;
+import com.kryptokrauts.aeternity.sdk.service.delegation.DelegationService;
+import com.kryptokrauts.aeternity.sdk.service.delegation.DelegationServiceFactory;
+import com.kryptokrauts.aeternity.sdk.service.name.domain.NameEntryResult;
+import com.kryptokrauts.aeternity.sdk.service.transaction.domain.PostTransactionResult;
+import com.kryptokrauts.aeternity.sdk.service.transaction.type.model.SpendTransactionModel;
+import com.kryptokrauts.aeternity.sdk.service.unit.UnitConversionService;
+import com.kryptokrauts.aeternity.sdk.service.unit.impl.DefaultUnitConversionServiceImpl;
+import com.kryptokrauts.aeternity.sdk.util.CryptoUtils;
+import com.kryptokrauts.contraect.generated.AensDelegation;
+import com.kryptokrauts.contraect.generated.AensDelegation.Address;
+import com.kryptokrauts.contraect.generated.AensDelegation.Hash;
+import com.kryptokrauts.contraect.generated.AensDelegation.Signature;
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import lombok.extern.slf4j.Slf4j;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AensDelegationTest extends BaseTest {
+
+  private static Random random = new Random();
+
+  private static AensDelegation aensDelegationInstance;
+
+  private static KeyPair nameOwnerKeyPair;
+
+  private static DelegationService delegationService;
+
+  private static UnitConversionService unitConversionService =
+      new DefaultUnitConversionServiceImpl();
+
+  private static String delegationTestName = "delegationTestName" + random.nextInt() + ".chain";
+
+  private static BigInteger salt;
+
+  private static Hash commitmentHash;
+
+  private static Address nameOwnerAddress;
+
+  private static Signature aensDelegationSignature;
+
+  @BeforeAll
+  public static void deployAndFund() {
+    aensDelegationInstance = new AensDelegation(config, null);
+    // deploy contract
+    Pair<String, String> deployment = aensDelegationInstance.deploy();
+    String txHash = deployment.getValue0();
+    log.info("tx-hash of deployment: {}", txHash);
+    log.info("contract id: {}", deployment.getValue1());
+    Assertions.assertNotNull(aensDelegationInstance.getContractId());
+
+    // fund name owner keypair
+    nameOwnerKeyPair = keyPairService.generateKeyPair();
+    // wrap into Address obj used for contract calls
+    nameOwnerAddress = new Address(nameOwnerKeyPair.getAddress());
+    log.info(nameOwnerKeyPair.toString());
+
+    AccountResult testAccount = aeternityService.accounts.blockingGetAccount();
+    SpendTransactionModel spendTransactionModel =
+        SpendTransactionModel.builder()
+            .amount(unitConversionService.toSmallestUnit("500")) // 500 AE
+            .nonce(testAccount.getNonce().add(BigInteger.ONE))
+            .recipient(nameOwnerKeyPair.getAddress())
+            .sender(config.getKeyPair().getAddress())
+            .build();
+    PostTransactionResult spendTxResult =
+        aeternityService.transactions.blockingPostTransaction(spendTransactionModel);
+    log.info("SpendTx result: {}", spendTxResult);
+
+    // initialize delegation service with correct config
+    delegationService =
+        new DelegationServiceFactory()
+            .getService(
+                ServiceConfiguration.configure()
+                    .network(Network.DEVNET)
+                    .keyPair(nameOwnerKeyPair)
+                    .compile());
+  }
+
+  @Test
+  @Order(1)
+  public void preClaimName() {
+    salt = CryptoUtils.generateNamespaceSalt();
+    commitmentHash = new Hash(delegationService.getAensCommitmentHash(delegationTestName, salt));
+    Signature preClaimSignature =
+        new Signature(
+            delegationService.createAensDelegationSignature(
+                aensDelegationInstance.getContractId()));
+    log.info("pre-claim delegation signature: {}", preClaimSignature.getSignature());
+    log.info("pre-claim name {} with salt {}", delegationTestName, salt);
+    String txHash =
+        aensDelegationInstance.pre_claim(nameOwnerAddress, commitmentHash, preClaimSignature);
+    log.info("pre-claim tx-hash: {}", txHash);
+  }
+
+  @Test
+  @Order(2)
+  public void claimName() {
+    aensDelegationSignature =
+        new Signature(
+            delegationService.createAensDelegationSignature(
+                aensDelegationInstance.getContractId(), delegationTestName));
+    log.info("aens delegation signature: {}", aensDelegationSignature.getSignature());
+    BigInteger nameFee = AENS.getInitialNameFee(delegationTestName);
+    log.info("nameFee: {}", nameFee);
+    log.info("claim name {} with salt {}", delegationTestName, salt);
+    String txHash =
+        aensDelegationInstance.claim(
+            nameOwnerAddress, delegationTestName, salt, nameFee, aensDelegationSignature);
+    log.info("claim tx-hash: {}", txHash);
+  }
+
+  @Test
+  @Order(3)
+  public void extendName() {
+    NameEntryResult nameEntryResult = aeternityService.names.blockingGetNameId(delegationTestName);
+    BigInteger oldTtl = nameEntryResult.getTtl();
+    BigInteger currentHeight = aeternityService.info.blockingGetCurrentKeyBlock().getHeight();
+    BigInteger newTtlHeight = currentHeight.add(BigInteger.valueOf(500));
+    String txHash =
+        aensDelegationInstance.extend(
+            nameOwnerAddress,
+            delegationTestName,
+            aensDelegationSignature,
+            Optional.of("FixedTTL(" + newTtlHeight.toString() + ")"));
+    log.info("extend tx-hash: {}", txHash);
+    nameEntryResult = aeternityService.names.blockingGetNameId(delegationTestName);
+    BigInteger newTtl = nameEntryResult.getTtl();
+    Assertions.assertNotEquals(oldTtl, newTtl);
+    Assertions.assertEquals(newTtlHeight, newTtl);
+  }
+
+  @Disabled // currently not working =>
+            // https://github.com/kryptokrauts/contraect-maven-plugin/issues/65
+  @Test
+  @Order(4)
+  public void addAndGetPointers() {
+    aensDelegationInstance.add_key(
+        nameOwnerAddress,
+        delegationTestName,
+        AENS.POINTER_KEY_ACCOUNT,
+        nameOwnerAddress.getAddress(),
+        aensDelegationSignature);
+    NameEntryResult nameEntryResult = aeternityService.names.blockingGetNameId(delegationTestName);
+    Assertions.assertEquals(1, nameEntryResult.getPointers().size());
+    Assertions.assertEquals(
+        nameOwnerAddress.getAddress(), nameEntryResult.getAccountPointer().get());
+    Map<String, Object> pointers = aensDelegationInstance.get_pointers(delegationTestName);
+    Assertions.assertEquals(1, pointers.size());
+    Assertions.assertEquals(nameOwnerAddress.getAddress(), pointers.get(AENS.POINTER_KEY_ACCOUNT));
+  }
+}
---
+++ src/test/java/com/kryptokrauts/OracleDelegationTest.java
@@ -0,0 +1,141 @@
+package com.kryptokrauts;
+
+import com.kryptokrauts.aeternity.sdk.constants.Network;
+import com.kryptokrauts.aeternity.sdk.domain.secret.KeyPair;
+import com.kryptokrauts.aeternity.sdk.service.ServiceConfiguration;
+import com.kryptokrauts.aeternity.sdk.service.account.domain.AccountResult;
+import com.kryptokrauts.aeternity.sdk.service.delegation.DelegationService;
+import com.kryptokrauts.aeternity.sdk.service.delegation.DelegationServiceFactory;
+import com.kryptokrauts.aeternity.sdk.service.oracle.domain.OracleQueryResult;
+import com.kryptokrauts.aeternity.sdk.service.oracle.domain.RegisteredOracleResult;
+import com.kryptokrauts.aeternity.sdk.service.transaction.domain.PostTransactionResult;
+import com.kryptokrauts.aeternity.sdk.service.transaction.type.model.SpendTransactionModel;
+import com.kryptokrauts.contraect.generated.OracleDelegation;
+import com.kryptokrauts.contraect.generated.OracleDelegation.Address;
+import com.kryptokrauts.contraect.generated.OracleDelegation.Oracle;
+import com.kryptokrauts.contraect.generated.OracleDelegation.Oracle_query;
+import com.kryptokrauts.contraect.generated.OracleDelegation.Signature;
+import java.math.BigInteger;
+import lombok.extern.slf4j.Slf4j;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OracleDelegationTest extends BaseTest {
+
+  private static OracleDelegation oracleDelegationInstance;
+
+  private static Oracle oracle;
+
+  private static BigInteger oracleTtl;
+
+  private static KeyPair oracleKeyPair;
+
+  private static DelegationService delegationService;
+
+  @BeforeAll
+  public static void deployAndFund() {
+    oracleDelegationInstance = new OracleDelegation(config, null);
+    // deploy contract
+    Pair<String, String> deployment = oracleDelegationInstance.deploy();
+    String txHash = deployment.getValue0();
+    log.info("tx-hash of deployment: {}", txHash);
+    log.info("contract id: {}", deployment.getValue1());
+    Assertions.assertNotNull(oracleDelegationInstance.getContractId());
+
+    // fund oracle keypair
+    oracleKeyPair = keyPairService.generateKeyPair();
+    AccountResult testAccount = aeternityService.accounts.blockingGetAccount();
+    SpendTransactionModel spendTransactionModel =
+        SpendTransactionModel.builder()
+            .amount(new BigInteger("1000000000000000000"))
+            .nonce(testAccount.getNonce().add(BigInteger.ONE))
+            .recipient(oracleKeyPair.getAddress())
+            .sender(config.getKeyPair().getAddress())
+            .build();
+    PostTransactionResult spendTxResult =
+        aeternityService.transactions.blockingPostTransaction(spendTransactionModel);
+    log.info("SpendTx result: {}", spendTxResult);
+
+    // initialize delegation service with correct config
+    delegationService =
+        new DelegationServiceFactory()
+            .getService(
+                ServiceConfiguration.configure()
+                    .network(Network.DEVNET)
+                    .keyPair(oracleKeyPair)
+                    .compile());
+  }
+
+  @Test
+  @Order(1)
+  public void createOracle() {
+    String signature =
+        delegationService.createOracleDelegationSignature(oracleDelegationInstance.getContractId());
+    Pair<String, Oracle> oracleRegisterTx =
+        oracleDelegationInstance.signedRegisterOracle(
+            new Address(oracleKeyPair.getAddress()),
+            new Signature(signature),
+            BigInteger.ONE,
+            "RelativeTTL(800)",
+            BigInteger.ZERO);
+    log.info("tx-hash of signedRegisterOracle: {}", oracleRegisterTx.getValue0());
+    oracle = oracleRegisterTx.getValue1();
+    log.info("{}", oracle);
+    RegisteredOracleResult registeredOracleResult =
+        aeternityService.oracles.blockingGetRegisteredOracle(oracle.getOracle());
+    log.info("{}", registeredOracleResult);
+    oracleTtl = registeredOracleResult.getTtl();
+  }
+
+  @Test
+  @Order(2)
+  public void extendOracle() {
+    String signature =
+        delegationService.createOracleDelegationSignature(oracleDelegationInstance.getContractId());
+    String oracleExtendTxHash =
+        oracleDelegationInstance.signedExtendOracle(
+            oracle, new Signature(signature), "RelativeTTL(800)", BigInteger.ZERO);
+    log.info("tx-hash of signedExtendOracle: {}", oracleExtendTxHash);
+    RegisteredOracleResult registeredOracleResult =
+        aeternityService.oracles.blockingGetRegisteredOracle(oracle.getOracle());
+    log.info("initial ttl: {}", oracleTtl);
+    log.info("new ttl after extending it: {}", registeredOracleResult.getTtl());
+    Assertions.assertEquals(
+        oracleTtl.add(BigInteger.valueOf(800)), registeredOracleResult.getTtl());
+  }
+
+  @Test
+  @Order(3)
+  public void createAndRespondToQuery() {
+    BigInteger queryFee = oracleDelegationInstance.queryFee(oracle);
+    log.info("query fee: {}", queryFee);
+    Pair<String, Oracle_query> createQueryTx =
+        oracleDelegationInstance.createQuery(
+            oracle,
+            "how is the wheather over there?",
+            "RelativeTTL(100)",
+            "RelativeTTL(100)",
+            queryFee);
+    Oracle_query query = createQueryTx.getValue1();
+    log.info("oracle query id: {}", query.getOracle_query());
+    String signature =
+        delegationService.createOracleDelegationSignature(
+            oracleDelegationInstance.getContractId(), query.getOracle_query());
+    String signedRespondTxHash =
+        oracleDelegationInstance.signedRespond(oracle, query, new Signature(signature), "sunny =)");
+    log.info("tx-hash of signedRespond: {}", signedRespondTxHash);
+    OracleQueryResult oracleQueryResult =
+        aeternityService.oracles.blockingGetOracleQuery(
+            oracleKeyPair.getOracleAddress(), query.getOracle_query());
+    // TODO https://github.com/kryptokrauts/aepp-sdk-java/issues/170
+    //    Assertions.assertEquals(
+    //        "sunny =)", EncodingUtils.decodeCheckWithIdentifier(oracleQueryResult.getResponse()));
+  }
+}
